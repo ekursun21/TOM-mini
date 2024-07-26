b2fafa33a3dfa43264d8f03e52dcdb5cca801506
Emre Kurşun <100401689+ekursun21@users.noreply.github.com>
2024-07-26 18:32:40 +0300

    first commit

---
+++ tom-mini/src/main/java/com/TOM/tom_mini/TomMiniApplication.java
@@ -0,0 +1,13 @@
+package com.TOM.tom_mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomMiniApplication.class, args);
+	}
+
+}
---
+++ tom-mini/src/test/java/com/TOM/tom_mini/TomMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TOM.tom_mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
